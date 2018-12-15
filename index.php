b4b3b31191cdc39b03777a2c40bc30734415fa88
Emmanuelle Husson <>
2018-12-15 14:53:50 +0100

    inclusion du header dans l'index, afficher chaque article avec son titre, bouton

--- index.php
+++ index.php
@@ -8,29 +8,18 @@ $articles = getArticles();
 
 include 'header.php';
 ?>
-
-	
-	   <div id="slide-accueil">
-		<div id="cbp-fwslider" class="cbp-fwslider">
-		<ul>
-			<li><a href="#"><img src="images/img1.jpg" alt="img01"/></a></li>
-			<li><a href="#"><img src="images/img2.jpg" alt="img02"/></a></li>
-			<li><a href="#"><img src="images/img3.jpg" alt="img03"/></a></li>
-		</ul>
-		</div>
-	   </div>
-	
-<!-----FIN SLIDER----->
 		
 		<div class="container">
-			<h1>Mon blog</h1>
+			
 			<?php foreach($articles as $article): ?>
 			<?php /*  print_r($article); */ ?>
 				<h2><?= $article->title ?></h2>
-				<time><?= $article->date ?></time>
-				<a href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Lire la suite</a>
-			<br/><hr/><br/>
+				<a id="buttoninfo" href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Lire l'article</a>
+				<br/><br/>_______ <br/>
 			<?php endforeach; ?>
 		</div>
+
+
 	</body>
+
 </html>
