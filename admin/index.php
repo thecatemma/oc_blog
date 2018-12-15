8b44c5d40370f915174670bbd2d739d9ed2ca7ad
Emmanuelle Husson <>
2018-12-15 15:55:53 +0100

    Mise a jour du fichier d'index du back office

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,5 @@
 <?php
 // Affiche la page d'accueil du blog qui liste le titre et lien cliquable des différents articles
-
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 require_once('./functions.php');
@@ -10,27 +9,24 @@ $articles = getArticles();
 
 <!DOCTYPE html>
 <html>
-	<head>
 
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Dernier Billet pour l'Alaska</title>
-		<link rel="stylesheet" href="blog.css">
-	</head>
 	
 <body>
 	<header>
 	</header>
 		<div class="container">
-			<h1>Mon blog</h1>
+			<h1>Espace d'administration</h1>
+			<hr/>
+			<a href="./formulaireAjout.php">Créer un nouvelle article</a>
+			<br/><hr/><br/>
+			<p><b>Liste des articles éxistants : </b></p>
 			<?php foreach($articles as $article): ?>
 			<?php /*  print_r($article); */ ?>
-				<h2><?= $article->title ?></h2>
-				<time><?= $article->date ?></time>
+				<p><?= $article->title ?>
+					<a href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Editer l'article</a>
+					<a href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Supprimer l'article</a>
+				</p>
 
-				<a href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Lire la suite</a>
-			<br/><hr/><br/>
-			
 			<?php endforeach; ?>
 		</div>
 </body>
