dfcb9294eb819800d324be8841423027c954d07a
Emmanuelle Husson <>
2018-12-15 14:39:50 +0100

    fichier connect.php qui permet de connecter les fichiers à BDD

--- connect.php
+++ connect.php
@@ -1,6 +1,5 @@
 <?php
-// NE PAS MODIFIER 
-// permet la connection à la base de donnée
+// permet la connection à la base de données
 
 $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
