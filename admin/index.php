1dad2e95d06d2f12baa82b27ce4091cf7b67c576
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-05 04:00:04 +0100

    Ajustement des fonctions par rapport à des fichiers supprimés

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,21 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 require_once('./functions.php');
 $articles = getArticles();
 
+
+require_once('./functions.php');
+
+if($_GET['method'] == 'delete'){
+	// FONCTION POUR SUPPRIMER LES ARTICLES
+	
+	$id=$_GET['id']; // on recupere dans l'URL le parametre ID
+
+	//createArticle('Mon Petit Poney', 'Que j\'aime beaucoup');
+	deleteArticle($id);
+	echo('article '.$id.' supprimé');
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -23,8 +38,8 @@ $articles = getArticles();
 			<?php foreach($articles as $article): ?>
 			<?php /*  print_r($article); */ ?>
 				<p><?= $article->title ?>
-					<a href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Editer l'article</a>
-					<a href="supprimerArticle.php?id=<?= $article->id ?>" class="btn btn-primary"> Supprimer l'article</a>
+					<a href="updateArticle.php?id=<?= $article->id ?>" class="btn btn-primary"> Editer l'article</a>
+					<a href="index.php?method=delete&id=<?= $article->id ?>" class="btn btn-primary"> Supprimer l'article</a>
 <!-- le lien redirige vers le fichier "supprimerArticle.php" avec l'identifiant de l'article à supprimer passé en paramètre -->
 				</p>
 
